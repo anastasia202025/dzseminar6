efccbd0a38a76c1c794f0e83af12188ec97ba4f6
Anastasia <anastasija2003@mail.ru>
2023-02-04 14:51:09 +0300

    seminar6dz

---
+++ 1/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 1: Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+int[] CreatArray(string message) // ф-ция создания массива с заданным пользователем размером
+{
+    System.Console.WriteLine(message + "-->");
+    int sizeMassiv= Convert.ToInt32(Console.ReadLine());
+    int[]massiv=new int [sizeMassiv];
+    return massiv;
+}
+int[] FillingArray(int[]baseArray,string message) // Ф-ция заполнения массива пользователем
+{
+    for(int i = 0; i < baseArray.Length; i++)
+    {
+        System.Console.WriteLine(message + $"-->");
+        baseArray[i]= Convert.ToInt32(Console.ReadLine());
+    }
+    return baseArray;
+}
+void DisplayElements(int[] baseArray) // Процедура вывовда индекса и присвоенного эл-та
+{
+    System.Console.WriteLine("Введенные числа: ");
+    for (int i= 0; i< baseArray.Length; i++)
+    {
+        System.Console.WriteLine($"{baseArray[i]}");
+
+    }
+}
+void FindNum(int[ ] baseArray) // Процедура проверки условия задачи и вывод результата
+{
+    int count = 0;
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+        if (baseArray[i]>0)
+        {
+            count++;
+        }
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    System.Console.WriteLine($"Количество чисел со значением  больше нуля составляет --> {count}");
+    Console.ResetColor();
+}
+
+int[]baseArray=CreatArray("Введите количество чисел для ввода");
+string message = $"Введите число";
+FillingArray(baseArray,message);
+DisplayElements(baseArray);
+FindNum(baseArray);
---
+++ 2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+void UserInput (double[] coeff, string message)
+{
+    System.Console.WriteLine($"Введите коэффициенты для уравнений k1,b1,k2,b2 ");
+    for(int i= 0;i<= coeff.Length-1; i++)
+    {
+        coeff[i]= Convert.ToDouble(Console.ReadLine());
+    }
+}
+
+double FindX (double[] coeff)
+{
+    double x = (coeff[3]-coeff[1])/(coeff[0]-coeff[2]);
+    return x;
+}
+double[]coeff= new double[4];
+UserInput(coeff, "Введите коэффициенты для расчета");
+double result = FindX(coeff);
+Console.ForegroundColor = ConsoleColor.Green;
+System.Console.WriteLine($"Координата X пересечения двух прямых с заданными коэффициентами -->{result}");
+Console.ResetColor();
